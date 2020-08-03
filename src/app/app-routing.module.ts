2fafd0d254a96d0cf01d2e347a2ca21eab383da5
David Jovellar <davidjovellar93@gmail.com>
2020-08-03 19:39:24 +0200

    Layout of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: BodyComponent},
+  { path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
