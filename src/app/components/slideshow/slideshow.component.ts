dd15d69284d0804d71aa5c158d00f2b21d5b7cca
David Jovellar <davidjovellar93@gmail.com>
2020-10-10 14:00:18 +0200

    Improve Slideshow

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Card } from 'src/app/models/card.model';
 import { Swiper } from 'swiper/bundle';
 
@@ -19,31 +19,32 @@ export class SlideshowComponent implements OnInit {
   ngOnInit(): void {
     this.configureSwiper();
   }
-  
   configureSwiper() {
     this.mySwiper = new Swiper('.swiper-container', {
       loop: true,
+      effect: 'coverflow',
       grabCursor: true,
       slidesPerView: 2,
-      spaceBetween: 50,
+      spaceBetween: 350,
       centeredSlides: true,
-      effect: 'coverflow',
+      observer: true,
+      observeParents: true,
+      parallax: true,
       coverflowEffect: {
-        rotate: 50,
+        rotate: 0,
         stretch: 0,
-        depth: 200,
+        depth: 500,
         modifier: 1,
         slideShadows : true,
       },
       pagination: {
         el: '.swiper-pagination',
-      },
+      }
     });
   }
 
   onSlideNext()
   {
-    console.log(this.cards);
     this.mySwiper.slideNext();
   }
 
